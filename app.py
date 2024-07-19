19cb4e4006017da2e810ffa3473526ee5fa0e81f
nassimminou <nassimmninou@outlook.fr>
2024-07-19 22:28:13 +0100

    habobob

--- app.py
+++ app.py
@@ -119,7 +119,8 @@ def extract_data(user_question):
     answer=response.content[0].text
     return answer
 def handle_userinput(user_question):
-    with open(os.path.join(os.path.dirname(__file__), 'prompt.txt'), 'r') as file:        system_prompt = file.read()
+    with open(os.path.join(os.path.dirname(__file__), 'prompt.txt'), 'r') as file:        
+        system_prompt = file.read()
         print("system wiwiw")
         print(system_prompt)
 # Create a message
